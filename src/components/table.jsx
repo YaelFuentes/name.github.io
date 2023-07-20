83a1cc5fcf15bb9d086c16d4c3f8518680e8a398
YaelFuentes <Yaelfuentes32@gmail.com>
2023-07-20 20:21:28 -0300

    create table, knex

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import SimpleModal from './Mui/modal';
 import axios from 'axios';
 
 export default function TableResponsive({ columns, rows, optional }) {
@@ -46,7 +45,6 @@ export default function TableResponsive({ columns, rows, optional }) {
       <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-
             <tr>
               {columns.map((i) => {
                 return (
